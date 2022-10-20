60ea7774ad316c7449fb1007c9eaa0ac05f12288
A-Bak <a-bak@users.noreply.github.com>
2022-10-20 15:05:31 +0200

    Added - simple scheduler.

--- app/sms_bot.py
+++ app/sms_bot.py
@@ -1,13 +1,35 @@
-from flask import Flask
+import time
+import atexit
 
-app = Flask(__name__)
+from flask import Flask
+from apscheduler.schedulers.background import BackgroundScheduler
+
+
+def print_current_time():
+    print(time.strftime("%A, %d. %B %Y %I:%M:%S %p"))
+
+
+def create_app() -> Flask:
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(func=print_current_time, trigger="interval", seconds=30)
+    scheduler.start()
+    atexit.register(lambda: scheduler.shutdown())
+
+    return Flask(__name__)
+
+
+app = create_app()
+app.config['DEBUG'] = False
 
 
 @app.route("/")
 def hello_world():
-    app.logger.info("Started SMS-Bot.")
     return "<p>Hello, this is SMS-Bot!</p>"
 
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    host = "127.0.0.1"
+    port = 5000
+    debug = False
+
+    app.run(host=host, port=port, debug=debug)
